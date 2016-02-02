fea7a710872a29fa2bbe140fb87d414c9a0ee07c
jbedia <joaquin.bedia@gmail.com>
2016-02-02 16:36:32 +0100

    Added index and measure definition templates

---
+++ inst/templates/index.template.R
@@ -0,0 +1,34 @@
+#' @title index.NameOfTheIndex
+#' 
+#' @description Function to compute NameOfTheIndex index.
+#'
+#' @param ts A time series of the target variable
+#' @param dates Calendar dates corresponding to the time series records
+#' @param ... Further parameters particular to the index definition, if any (e.g., threshold, probability, etc.)
+#' 
+#' @return Either a scalar or a vector depending on the index definition
+#' 
+#' @details  It is assumed that the input time series do not have any missing values (these are filtered previously).
+#'  Date formatting corresponds to a character string or POSIXct defining the calendar dates, of the form
+#'  \code{"%Y-%m-%d %H:%M:%S"}. See \code{\link{srtptime}} for further details on date formatting. 
+#' 
+#' @author corrresponding author \email{author@@email.com}, author2, ...
+#' 
+#' @export
+#'  
+#' @family validation
+#'
+#' @examples \dontrun{
+#' # Observed precipitation in Braganca:
+#' data(precipIberiaECA)
+#' obs <- precipIberiaECA$observations$Data[,1]
+#' index.value <- index.nameOfTheIndex(obs)
+#' }
+
+index.NameOfTheIndex <- function(ts, dates, ...) {
+      # Include the code needed to estimate the index
+      return(index)
+}
+
+
+
---
+++ inst/templates/measure.template.R
@@ -0,0 +1,26 @@
+#' @title measure.NameOfTheMeasure
+#' @description Function to compute the NameOfTheMeasure. The measure depends on the index nameOfTheIndex.
+#' @param obs Index value for the observed series or vector with the observed time series. 
+#' @param pred Index value for the predicted series or vector with the predicted time series. 
+#' @param dates
+#' @param ... Additional parameters required by a particular measure
+#' @details Some measures are computed directly from the original time series (e.g. temporal correlation),
+#' whereas others are computed upon previouly computed indices (e.g. mean bias).
+#' @return A scaler or a vector corresponding to the measure value(s).
+#' @export
+#' @family validation
+#' @author corrresponding author \email{author@@email.com}, author2, ...
+#' @examples \dontrun{
+#' # Measure Braganca:
+#' data(precipIberiaECA)
+#' obs <- precipIberiaECA$observations$Data[,1]
+#' prd <- precipIberiaECA$observations$Data[,2]
+#' obsMean <- nameMeasure.value(obs,prd)
+#' str(obsMean)
+#' }
+
+# Measure:
+nameMeasure.value <- function(obs, pred, ...){
+      # Include the code needed to estimate the measure
+      return(measure)
+}
