432d118dfcb9397dec04a8462f976ecf9f9a1f91
jbedia <joaquin.bedia@gmail.com>
2016-02-17 14:09:15 +0100

    Added neasure.ratio

---
+++ R/measure.ratio.R
@@ -0,0 +1,11 @@
+#' @title Ratio
+#' @description Function to compute the ratio between the predicted and observed means. 
+#' The measure depends on the index mean.
+#' @author J. Bedia, D. San-Mart\'in, S. Herrera
+#' @template templateMeasureParams
+#' @return A float number corresponding to the ratio.
+#' @export
+
+measure.ratio <- function(indexObs = NULL, indexPrd = NULL, obs = NULL, prd = NULL) {
+      indexPrd / indexObs
+}
