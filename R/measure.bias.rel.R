2d6368e3229c54b7db7173bd4eda86fa260bb2ff
jbedia <joaquin.bedia@gmail.com>
2016-02-17 14:10:17 +0100

    Added relative bias measure

---
+++ R/measure.bias.rel.R
@@ -0,0 +1,11 @@
+#' @title Relative bias
+#' @description Function to compute the difference between the relative bias between the predicted and observed means. 
+#' The measure depends on the index mean.
+#' @author J. Bedia, D. San-Mart\'in, S. Herrera
+#' @template templateMeasureParams
+#' @return A float number corresponding to the bias.
+#' @export
+
+measure.bias.rel <- function(indexObs = NULL, indexPrd = NULL, obs = NULL, prd = NULL) {
+      (indexPrd - indexObs) / indexPrd
+}
