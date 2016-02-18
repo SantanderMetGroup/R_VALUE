116939404d6f19b5aa4f4d2237c0205ec92ff6be
jbedia <joaquin.bedia@gmail.com>
2016-02-18 11:32:23 +0100

    Added new low frequency var routine

---
+++ R/index.lfvar.R
@@ -0,0 +1,17 @@
+#' @title Proportion of variance in low frequency
+#' @description Annual and seasonal proportion of variance in low frequency
+#' @template templateIndexParams
+#' @param lowVarPeriod Low frequency threshold. Minimum cycles per unit time below which frequency is considered as low
+#' @param INDEX A Vector defining the aggregation periods (passed to \code{tapply}). No aggregation is performed by default.
+#' @author Douglas Maraun \email{dmaraun@@geomar.de}
+#' @export
+
+index.lfvar <- function(ts, lowVarPeriod, INDEX = 1:length(ts)) {
+      mean.x <- tapply(ts, INDEX, mean, na.rm = TRUE)
+      specVar <- spec.pgram(mean.x, na.action = na.exclude, plot = FALSE)
+      lowfreqvar <- sum(specVar$spec[1/specVar$freq >= lowVarPeriod], na.rm = TRUE)
+      totalvar <- sum(specVar$spec, na.rm = TRUE)
+      lowfreqvar / totalvar
+}
+
+
