4610bcaa973380402ec3dd027ce861c5fc0049f9
jbedia <joaquin.bedia@gmail.com>
2016-03-09 10:32:33 +0100

    Bug fix and other minor changes

--- R/corrMat.VALUE.R
+++ R/corrMat.VALUE.R
@@ -23,26 +23,38 @@
 #'  Default to NULL, meaning that the correlation matrix is done on the observations.
 #' @param season Character string indicating the target season. Accepted values are 
 #' \code{c("annual", "DJF", "MAM", "JJA", "SON")}. Several choices in the same request are accepted (all are computed by default).
-#' @param method Correlation method, passed to the \code{\link{cor}} function
+#' @param method Correlation method, passed to the \code{\link{cor}} function. Default is \code{"spearman"}, but
+#' \code{"pearson"} and \code{"kendall"} are also accepted.
 #' @param type Type of aggregation in the case of multiple realizations. Should the aggregation of 
-#' multiple members be performed \code{"before"} (default) or \code{"after"} computing the correlations?. Ignored
+#' multiple members be performed \code{"before"} or \code{"after"} (the default) computing the correlations?. Ignored
 #'  in the case of observations and deterministic predictions.
 #' @param max.na.prop Maximum allowed proportion of missing data (Default to 0.25). See details
 #' @param deseason Logical. Should the seasonal cycle from the input data series be removed?. See details.
 #' @param window.width Used if \code{deseason = TRUE}. Integer number indicating the width, in days, of the window used for
 #'  moving average computation of the reference daily climatology. Default to 31 days. See details.
-#' @details A (circular) moving average daily climatology can be automatically calculated for each data series, considering 
-#' a specific window width (31 days by default, centered around lag 0). This is used for removing the seasonal cycles when applicable. 
-#' An additional argument allows to specify the maximum number of missing values allowed, being ommited from the analysis 
-#' those data series above the threshold.
+#' @details 
+#' 
+#' \strong{Seasonal cycle removal}
+#' 
+#' A (circular) moving average daily climatology can be automatically calculated for each data series, considering 
+#' a specific window width (31 days by default, centered around lag 0). This is used for removing the seasonal cycle. 
+#' 
+#' \strong{Missing data treatment}
+#' 
+#' An additional argument, \code{max.na.prop}, allows to specify the maximum number of missing values allowed, being ommited from the analysis 
+#' those data series above the threshold. Note that this argument is applied on a seasonal basis, so correlation
+#' matrices for different seasons may differ in their dimension. Missing ata filering can be disabled by setting 
+#' the maximum allowed proportion of missing data to 1.
+#' 
+#' 
 #' @return A list of 2D matrices. The length of the list corresponds to the periods indicated in the \code{season} argument (default to 5,
 #'  annual and the four standard WMO seasons). Matrix attributes indicate the station names (in the row/column order they appear),
-#'   and their geographical coordinates.   
+#'   and their geographical coordinates. Global attributes provide other method details.   
 #' @author J. Bedia 
 #' @export
 #' @examples \dontrun{
 #' obs.file <- file.path(find.package("R.VALUE"),"example_datasets","VALUE_ECA_86_v2.zip")
-#' stationObj <- loadValueStations(obs.file, "tmin")
+#' stationObj <- loadValueStations(obs.file, "precip")
 #' predictions.file <- file.path(find.package("R.VALUE"),"example_datasets",
 #'                                   "example_predictions_portal_exp1a_deterministic.zip")
 #' # Correlation matrix of annual data:                                  
@@ -50,20 +62,20 @@
 #' # Correlation matrix for winter and summer:
 #' djfjja <- corrMat.VALUE(stationObj, predictions.file, season = c("DJF","JJA"))
 #' }
-# 
+
 
 corrMat.VALUE <- function(stationObj,
                           predictions.file = NULL,
                           season = c("annual", "DJF", "MAM", "JJA", "SON"),
-                          method = c("pearson", "kendall", "spearman"),
-                          type = c("before", "after"),
+                          method = "pearson",
+                          type = "after",
                           max.na.prop = 0.25,
                           deseason = TRUE,
                           window.width = 31) {
       season <- match.arg(season, choices = c("annual", "DJF", "MAM", "JJA", "SON"), several.ok = TRUE)
       method <- match.arg(method, choices = c("pearson", "kendall", "spearman"))
-      type <- match.arg(type, choices = c("before", "after"))
-      o <- stationObj
+      type <- match.arg(type, choices = c("after", "before"))
+      o <- dimFix(stationObj)
       stationObj <- NULL
       if (!is.null(predictions.file)) {
             message("[", Sys.time(), "] - Loading predictions...")
@@ -89,7 +101,7 @@ corrMat.VALUE <- function(stationObj,
                           "MAM" = 3:5,
                           "JJA" = 6:8,
                           "SON" = 9:11)
-            aux <- dimFix(na.filter.VALUE(subsetVALUE(o, season = sea)))
+            aux <- na.filter.VALUE(subsetVALUE(o, season = sea), max.na.prop)
             # Correlation matrices for each member
             message("[", Sys.time(), "] - Computing correlation matrix for ", season[i], " period...")
             cor.mat.list <- lapply(1:n.mem, function(x) cor(aux$Data[x,,], use = "pairwise.complete.obs", method = method))
@@ -106,6 +118,10 @@ corrMat.VALUE <- function(stationObj,
       })
       names(mat.list) <- season
       message("[", Sys.time(), "] - Done.")
+      attr(mat.list, "correlation.type") <- method
+      attr(mat.list, "max.na.prop") <- max.na.prop
+      attr(mat.list, "deseason") <- deseason
+      if (deseason) attr(mat.list, "ma.window") <- window.width
       return(mat.list)
 }
 
