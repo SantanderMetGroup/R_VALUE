e035bd11bcddcdf1ec0067aa6222b70ac144e337
jbedia <joaquin.bedia@gmail.com>
2016-04-11 18:08:40 +0200

    Doc update

--- R/jointProbMat.VALUE.R
+++ R/jointProbMat.VALUE.R
@@ -43,12 +43,7 @@
 #' so the analysis of dependencies will be modulated by the different climatologies.
 #' In order to avoid this, the mutual information of 
 #' two random variables is a measure of the mutual dependence between the two variables.
-#'      
-#' If the two events occur together exactly as frequently as one would
-#' expect by chance, the ratio inside the log is equal to 1, giving a value of zero;
-#'  if they occur more frequently than one would expect by chance, the ratio is greater than 1
-#'  so MI > 0; and conversely if they occur less frequently than one would expect by chance, the ratio
-#' is less than 1 so MI < 0. 
+#' MI = 0 if the two events are independent.
 #' 
 #' Not limited to real-valued random variables like the correlation coefficient,
 #'  MI is more general and determines how similar the joint distribution p(X,Y) is to the products
