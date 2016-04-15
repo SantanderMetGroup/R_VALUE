9dbe52cfc957c7c760c8598ae944df735f7b079c
danielsms <daniel@predictia.es>
2016-04-15 12:01:54 +0200

    Create measure.nDays.R

---
+++ R/measure.nDays.R
@@ -0,0 +1,10 @@
+#' @title N days
+#' @description Function to count the sample size (number of days) used in the calculation of the indices
+#' @template templateMeasureParams
+#' @return An integer corresponding to the counts
+#' @author J. Bedia, D. San-Martin
+#' @export
+
+measure.nDays <- function(indexObs = NULL, indexPrd = NULL, obs, prd) {
+      length(obs)
+}
