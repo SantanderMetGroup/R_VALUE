437323a493f509bccd5728f74d26e4af7c089f49
jbedia <joaquin.bedia@gmail.com>
2016-06-24 17:18:54 +0200

    Added new type options in index.annual.cycle

--- R/index.annual.cycle.R
+++ R/index.annual.cycle.R
@@ -3,22 +3,39 @@
 #' @template templateIndexParams
 #' @template templateDates
 #' @param type Character string indicating the statistic. Currently implemented options are \code{"min"},
-#' \code{"max"}, \code{"amp"} for amplitude, and \code{"relamp"} for relative amplitude (in \%).
-#' @return A float number with the corresponding statistis.
-#' @author Sven Kotlarski, S. Herrera, J. Bedia, D. San Martin
+#' \code{"max"}, \code{"amp"} for amplitude, \code{"relamp"} for relative amplitude (in \%),
+#' \code{"phase"} for amplitude of the filtered time series and \code{"asymmetry"} to calculate the
+#' difference in days between maximum and minimum of the filtered cycle (see Details for the last two types).
+#' @details \strong{Time series filter}
+#' 
+#' The \code{type = "phase"} and \code{"asymmetry"} options apply a circular moving average, with a window width of 31 days
+#'  and centered on lag-0, and then return the day of the year (julian day [1-366]) reaching either the maximum or the number 
+#' of days between maximum and minimum respectively.
+#' 
+#' @return A float number with the corresponding statistics.
+#' @author Sven Kotlarski, S. Herrera, J. Bedia, D. San Martin, D. Maraun
 #' @export
+#' @importFrom stats filter
 
-index.annual.cycle <- function(ts, dates, type = c("min", "max", "amp", "relamp")) {
-      type <- match.arg(type, choices = c("min", "max", "amp", "relamp"))
-      # date format yyyy-mm-dd hh:mm:ss is assumed
-      # this speeds up the POSIXlt function which uses the slow strptime function
-      # providing a format and timezone to POSIXlt helps but this approach is faster
+index.annual.cycle <- function(ts, dates, type = c("phase","min", "max", "amp", "relamp", "asymmetry")) {
+      type <- match.arg(type, choices = c("phase", "min", "max", "amp", "relamp", "asymmetry"))
       doy <- substr(dates,6,10)
       ref <- tapply(ts, INDEX = doy, FUN = mean, na.rm = TRUE)
-      switch(type, 
+      if (type == "phase" | type == "asymmetry") {
+            ref <- filter(ref, 1/31 * rep(1, 31), method = "convolution", sides = 2, circular = TRUE)
+            if (type == "asymmetry") nmonth <- length(unique(substr(dates,6,7)))
+      }
+      switch(type,
              "min" = min(ref, na.rm = TRUE),
              "max" = max(ref, na.rm = TRUE),
              "amp" = max(ref, na.rm = TRUE) - min(ref, na.rm = TRUE),
-             "relamp" = (max(ref, na.rm = TRUE) - min(ref, na.rm = TRUE))*100 / mean(ref, na.rm = TRUE)
+             "relamp" = (max(ref, na.rm = TRUE) - min(ref, na.rm = TRUE))*100 / mean(ref, na.rm = TRUE),
+             "phase" = which.max(ref),
+             "asymmetry" = ((abs(which.max(ref) - which.min(ref)) * -1) %% 365) / nmonth
       )
 }
+
+
+      
+
+
