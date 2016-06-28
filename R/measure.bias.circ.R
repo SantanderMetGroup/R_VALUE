0531c8728f2508738e877084262d2c0aa0d2e794
danielsms <daniel@predictia.es>
2016-06-28 14:13:37 +0200

    Update measure.bias.circ.R
    Support for NA and NULL

--- R/measure.bias.circ.R
+++ R/measure.bias.circ.R
@@ -6,6 +6,12 @@
 #' @export
 
 measure.bias.circ <- function(indexObs = NULL, indexPrd = NULL, obs = NULL, prd = NULL) {
+      if(is.null(indexObs) | is.null(indexPrd)){
+            return(NA)
+      }
+      if(is.na(indexObs) | is.na(indexPrd)){
+            return(NA)
+      }
       distance <- (abs(indexObs - indexPrd) * -1) %% 365
       if (indexObs < indexPrd) {
             distance <- distance * -1
