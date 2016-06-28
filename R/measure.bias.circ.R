72917d07cd4b33496373aabf0efe36dce4dc7bd4
danielsms <daniel@predictia.es>
2016-06-28 15:35:03 +0200

    Update measure.bias.circ.R
    Bug fixed

--- R/measure.bias.circ.R
+++ R/measure.bias.circ.R
@@ -1,19 +1,15 @@
-#' @title Circular bias
-#' @description Function to compute the bias considering circular series
-#' @template templateMeasureParams
-#' @return A float number corresponding to the circular bias.
-#' @author D. San-Martin
-#' @export
-
-measure.bias.circ <- function(indexObs = NULL, indexPrd = NULL, obs = NULL, prd = NULL) {
+measure.bias.circ2 <- function(indexObs = NULL, indexPrd = NULL, obs = NULL, prd = NULL) {
       if(is.null(indexObs) | is.null(indexPrd)){
             return(NA)
       }
       if(is.na(indexObs) | is.na(indexPrd)){
             return(NA)
       }
-      distance <- (abs(indexObs - indexPrd) * -1) %% 365
-      if (indexObs < indexPrd) {
+      distance <- abs(indexPrd-indexObs) %% 365
+      if(distance > 365/2){
+            distance = 365 - distance
+      }
+      if(indexObs < indexPrd){
             distance <- distance * -1
       }
       return(distance)
