6dfb5813f9ba09c49030ab830407d949de94e799
jbedia <joaquin.bedia@gmail.com>
2017-04-06 10:39:52 +0200

    Speedup with fastLm

--- R/index.annual.cycle.filtered.R
+++ R/index.annual.cycle.filtered.R
@@ -7,8 +7,10 @@
 #' @param type A character string indicating max \code{"phase"} (days, default), \code{"amp"} (amplitude) or \code{"amprel"} (relative amplitude)
 #' @param thresh Threshold that defines weak and double maxima (relative to amplitude of annual cycle)
 #' @return A float number corresponding to the amplitude or the (phase) day of the maxima
+#' @importFrom RcppEigen fastLm
 #' @export
 #' @keywords internal
+
 
 index.annual.cycle.filtered <- function(ts, dates, type = 'phase', peak = 1, thresh = .1) {
       type <- match.arg(type, choices = c("phase", "amp", "amprel"))
@@ -24,7 +26,7 @@ index.annual.cycle.filtered <- function(ts, dates, type = 'phase', peak = 1, thr
       ch3 <- cos(t/T*6*pi)
       sh4 <- sin(t/T*8*pi)
       ch4 <- cos(t/T*8*pi)
-      mod4 <- lm(anncyc~sh1+ch1+sh2+ch2+sh3+ch3+sh4+ch4)
+      mod4 <- fastLm(anncyc~sh1+ch1+sh2+ch2+sh3+ch3+sh4+ch4)
       ac <- predict(mod4)
       n <- length(ac)
       ## to ease identification of maxima at beginning/end of year
@@ -36,13 +38,13 @@ index.annual.cycle.filtered <- function(ts, dates, type = 'phase', peak = 1, thr
       mmp <- mmt <- mmv <- NA
       nm <- 0
       for (i in 1 + 1:n) {
-            if (acdum[i - 1] < acdum[i] & acdum[i + 1] < acdum[i]) { ##max
+            if (acdum[i - 1] < acdum[i] & acdum[i + 1] < acdum[i]) { 
                   nm <- nm + 1
                   mmp[nm] <- i - 1
                   mmt[nm] <- 1
                   mmv[nm] <- ac[mmp[nm]]
             }
-            if (acdum[i - 1] > acdum[i] & acdum[i + 1] > acdum[i]) { ##max
+            if (acdum[i - 1] > acdum[i] & acdum[i + 1] > acdum[i]) {
                   nm <- nm + 1
                   mmp[nm] <- i - 1
                   mmt[nm] <- -1
@@ -51,45 +53,45 @@ index.annual.cycle.filtered <- function(ts, dates, type = 'phase', peak = 1, thr
       }
       ## merge double peaks
       if (nm > 2) {
-            if (mmt[2] == 1 & mmt[nm] == 1){
-                  if (abs(ac[mmp[2]] - ac[mmp[nm]])<thresh*amp & abs(mean(c(ac[mmp[2]],ac[mmp[nm]]))-ac[mmp[1]])<thresh*amp) {
+            if (mmt[2] == 1 & mmt[nm] == 1) {
+                  if (abs(ac[mmp[2]] - ac[mmp[nm]]) < thresh*amp & abs(mean(c(ac[mmp[2]], ac[mmp[nm]])) - ac[mmp[1]]) < thresh*amp) {
                         mmt[1] <- 1
                         mmt[nm] <- 0
                         mmt[2] <- 0
                         mmv[nm] <- 0
                         mmv[2] <- 0
-                        mmp[1] <- mean(c(mmp[nm]-366,mmp[2]))
-                        if (mmp[1]<1){
-                              mmp[1] <- mmp[1]+366
+                        mmp[1] <- mean(c(mmp[nm] - 366, mmp[2]))
+                        if (mmp[1] < 1) {
+                              mmp[1] <- mmp[1] + 366
                         }
-                        mmv[1] <- mean(c(ac[mmp[2]],ac[mmp[nm]]))
+                        mmv[1] <- mean(c(ac[mmp[2]], ac[mmp[nm]]))
                   }
             }
-            for(i in 2:(nm-1)){
-                  if (mmt[i-1]==1 & mmt[i+1]==1){
-                        if(abs(ac[mmp[i+1]]-ac[mmp[i-1]])<thresh*amp & abs(mean(c(ac[mmp[i+1]],ac[mmp[i-1]]))-ac[mmp[i]])<thresh*amp){
+            for (i in 2:(nm - 1)) {
+                  if (mmt[i - 1] == 1 & mmt[i + 1] == 1) {
+                        if (abs(ac[mmp[i + 1]] - ac[mmp[i - 1]]) < thresh*amp & abs(mean(c(ac[mmp[i + 1]], ac[mmp[i - 1]])) - ac[mmp[i]]) < thresh*amp) {
                               mmt[i] <- 1
-                              mmt[i-1] <- 0
-                              mmt[i+1] <- 0
-                              mmv[i-1] <- 0
-                              mmv[i+1] <- 0
-                              mmp[i] <- mean(c(mmp[i+1],mmp[i-1]))
-                              mmv[i] <- mean(c(ac[mmp[i+1]],ac[mmp[i-1]]))
+                              mmt[i - 1] <- 0
+                              mmt[i + 1] <- 0
+                              mmv[i - 1] <- 0
+                              mmv[i + 1] <- 0
+                              mmp[i] <- mean(c(mmp[i + 1], mmp[i - 1]))
+                              mmv[i] <- mean(c(ac[mmp[i + 1]], ac[mmp[i - 1]]))
                         }
                   }
             }
-            if (mmt[1]==1 & mmp[nm-1]==1){
-                  if(abs(ac[mmp[1]]-ac[mmp[nm-1]])<thresh*amp & abs(mean(c(ac[mmp[1]],ac[mmp[nm-1]]))-ac[mmp[nm]])<thresh*amp){
+            if (mmt[1] == 1 & mmp[nm - 1] == 1) {
+                  if (abs(ac[mmp[1]] - ac[mmp[nm - 1]]) < thresh*amp & abs(mean(c(ac[mmp[1]], ac[mmp[nm - 1]])) - ac[mmp[nm]]) < thresh*amp) {
                         mmt[nm] <- 1
-                        mmt[nm-1] <- 0
+                        mmt[nm - 1] <- 0
                         mmt[1] <- 0
-                        mmv[nm-1] <- 0
+                        mmv[nm - 1] <- 0
                         mmv[1] <- 0
-                        mmp[nm] <- mean(c(mmp[nm-1]-366,mmp[1]))
-                        if(mmp[nm]<1){
-                              mmp[nm] <- mmp[nm]+366
+                        mmp[nm] <- mean(c(mmp[nm - 1] - 366, mmp[1]))
+                        if (mmp[nm] < 1) {
+                              mmp[nm] <- mmp[nm] + 366
                         }
-                        mmv[nm] <- mean(c(ac[mmp[1]],ac[mmp[nm-1]]))
+                        mmv[nm] <- mean(c(ac[mmp[1]], ac[mmp[nm - 1]]))
                   }
             }
       }
@@ -100,44 +102,44 @@ index.annual.cycle.filtered <- function(ts, dates, type = 'phase', peak = 1, thr
       mmp <- mmp[itn0]
       nm <- length(mmt)
       ## delete weak peaks
-      if(nm>2){
+      if (nm > 2) {
             izero <- NA
             nzero <- 0
-            if(((abs(mmv[1]-mmv[nm])<thresh*amp) || (abs(mmv[1]-mmv[2])<thresh*amp)) & mmv[1]!=max(mmv)){
+            if (((abs(mmv[1] - mmv[nm]) < thresh*amp) || (abs(mmv[1] - mmv[2]) < thresh*amp)) & mmv[1] != max(mmv)) {
                   mmt[1] <- 0
-                  nzero <- nzero+1
+                  nzero <- nzero + 1
                   izero[nzero] <- 1
             }
-            for(i in 2:(nm-1)){
-                  if (((abs(mmv[i]-mmv[i-1])<thresh*amp) || (abs(mmv[i]-mmv[i+1])<thresh*amp)) & mmv[i]!=max(mmv)){
+            for (i in 2:(nm - 1)) {
+                  if (((abs(mmv[i] - mmv[i - 1]) < thresh*amp) || (abs(mmv[i] - mmv[i + 1]) < thresh*amp)) & mmv[i] != max(mmv)) {
                         mmt[i] <- 0
-                        nzero <- nzero+1
+                        nzero <- nzero + 1
                         izero[nzero] <- i
                   }
             }
-            if((abs(mmv[nm]-mmv[1])<thresh*amp) || (abs(mmv[nm]-mmv[nm-1])<thresh*amp) & mmv[nm]!=max(mmv)){
+            if ((abs(mmv[nm] - mmv[1]) < thresh*amp) || (abs(mmv[nm] - mmv[nm - 1]) < thresh*amp) & mmv[nm] != max(mmv)) {
                   mmt[nm] <- 0
-                  nzero <- nzero+1
+                  nzero <- nzero + 1
                   izero[nzero] <- nm
             }
-            for(i in 1:nzero) {
+            for (i in 1:nzero) {
                   mmv[izero[i]] <- 0
             }
       }
-      if(nm>2){
-            nmax <- sum(mmt==1)
-            if(nmax>=2){
-                  max2 <- sort(mmv[mmt==1])[nmax-1]
-                  pmax <- mmp[which(mmv>=max2 & mmt==1)]
-                  vmax <- mmv[which(mmv>=max2 & mmt==1)]
-                  if (vmax[2]>vmax[1])
+      if (nm > 2) {
+            nmax <- sum(mmt == 1)
+            if (nmax >= 2) {
+                  max2 <- sort(mmv[mmt == 1])[nmax - 1]
+                  pmax <- mmp[which(mmv >= max2 & mmt == 1)]
+                  vmax <- mmv[which(mmv >= max2 & mmt == 1)]
+                  if (vmax[2] > vmax[1])
                         pmax <- pmax[c(2,1)]
                   
-            }else{
-                  pmax <- mmp[which(mmv==max(mmv))]
+            } else {
+                  pmax <- mmp[which(mmv == max(mmv))]
             }
-      }else{
-            pmax <- mmp[which(mmv==max(mmv))]
+      } else {
+            pmax <- mmp[which(mmv == max(mmv))]
       }
       if (type == 'phase') {
             if (peak == 1) {
@@ -151,7 +153,7 @@ index.annual.cycle.filtered <- function(ts, dates, type = 'phase', peak = 1, thr
             }
       } else if (type == 'amp') {
             return(amp)
-      } else {
+      } else if (type == 'amprel') {
             return(amprel)
       }
 }
